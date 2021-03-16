f9d7486d56e0004054fff0855fdc04aa461f8f63
luiz motta <luizmotta01@hotmail.com>
2021-03-16 20:33:14 -0300

    feat: add infrastructure base scope

---
+++ DesafioWarren.Infrastructure/EntityFramework/Repositories/AccountsRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using DesafioWarren.Domain.Aggregates;
+using DesafioWarren.Domain.Repositories;
+using DesafioWarren.Domain.UnitOfWork;
+using DesafioWarren.Infrastructure.EntityFramework.DbContexts;
+
+namespace DesafioWarren.Infrastructure.EntityFramework.Repositories
+{
+    public class AccountsRepository : IAccountRepository
+    {
+        private readonly AccountsDbContext _context;
+
+        public IUnitOfWork UnitOfWork => _context;
+        
+        public AccountsRepository(AccountsDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Account account)
+        {
+            _context.Set<Account>().Add(account);
+        }
+
+        public void AddRange(IEnumerable<Account> accounts)
+        {
+            _context.Set<Account>().AddRange(accounts);
+        }
+
+        public void Remove(Account account)
+        {
+            _context.Set<Account>().Remove(account);
+        }
+
+        public void RemoveRange(IEnumerable<Account> accounts)
+        {
+            _context.Set<Account>().RemoveRange(accounts);
+        }
+    }
+}
