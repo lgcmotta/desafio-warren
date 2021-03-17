1b757e5f5abf6682ef5360dd2fdcab5487ce09c5
luiz motta <luizmotta01@hotmail.com>
2021-03-17 19:21:33 -0300

    feat: configure validation behaviour

--- DesafioWarren.Application/Behaviours/ValidationBehaviour.cs
+++ DesafioWarren.Application/Behaviours/ValidationBehaviour.cs
@@ -1,14 +1,47 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using DesafioWarren.Application.Models;
+using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace DesafioWarren.Application.Behaviours
 {
-    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TResponse : Response
     {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        private readonly ILogger<ValidationBehaviour<TRequest, TResponse>> _logger;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidationBehaviour<TRequest, TResponse>> logger)
+        {
+            _validators = validators;
+            _logger = logger;
+        }
+
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            return default;
+            var validationFailures = _validators
+                .Select(validator => validator.Validate(request))
+                .SelectMany(validationResult => validationResult.Errors)
+                .Where(validationFailure => validationFailure is not null)
+                .ToList();
+
+            return validationFailures.Any() ? CreateErrorResponse(validationFailures) : await next();
+        }
+
+        private static TResponse CreateErrorResponse(IEnumerable<ValidationFailure> validationFailures)
+        {
+            var response = new Response();
+
+            response.AddValidationFailures(validationFailures);
+
+            return response as TResponse;
         }
     }
+
 }
---
+++ DesafioWarren.Application/Models/Response.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
+namespace DesafioWarren.Application.Models
+{
+    public class Response
+    {
+        public object Payload { get; set; }
+        
+        public List<ValidationFailure> Failures { get; } = new();
+        
+        public Response(object payload)
+        {
+            Payload = payload;
+        }
+
+        public Response()
+        {
+                
+        }
+
+        public void AddValidationFailure(ValidationFailure validationFailure) => Failures.Add(validationFailure);
+        
+        public void AddValidationFailures(IEnumerable<ValidationFailure> validationFailures) => Failures.AddRange(validationFailures);
+
+        public void RemoveValidationFailure(ValidationFailure validationFailure) => Failures.Remove(validationFailure);
+
+        public void ClearValidationErrors() => Failures.Clear();
+        
+        public bool IsErrorResponse() => Failures.Any();
+    }
+  
+}
