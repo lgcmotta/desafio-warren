7dc87f2591a9af41d560deda8614d23cbd29875b
luiz motta <luizmotta01@hotmail.com>
2021-03-17 18:34:50 -0300

    feat: create automapper profiles and extensions

---
+++ DesafioWarren.Application/AutoMapper/Profiles/AccountsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DesafioWarren.Application.Models;
+using DesafioWarren.Domain.Aggregates;
+using DesafioWarren.Domain.Entities;
+
+namespace DesafioWarren.Application.AutoMapper.Profiles
+{
+    public class AccountsProfile : Profile
+    {
+        public AccountsProfile()
+        {
+            CreateMap<Account, AccountModel>()
+                .ForMember(accountModel => accountModel.Balance
+                    , options => options.MapFrom(account => account.GetBalance()))
+                .ReverseMap();
+
+            CreateMap<AccountTransaction, AccountTransactionModel>()
+                .ReverseMap();
+        }
+    }
+}
