8be643805e9c17b74a20d8e971a879553fdbaea7
luiz motta <luizmotta01@hotmail.com>
2021-03-17 18:54:21 -0300

    feat: configure loggin behaviour

--- DesafioWarren.Application/Behaviours/LogginBehaviour.cs
+++ DesafioWarren.Application/Behaviours/LogginBehaviour.cs
@@ -1,14 +1,32 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using DesafioWarren.Application.Extensions;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace DesafioWarren.Application.Behaviours
 {
     public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
+        private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+
+        public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            return default;
+            var commandName = request.GetGenericTypeName();
+
+            _logger.LogInformation("Handling command '{CommandName}'", commandName);
+
+            var response = await next();
+
+            _logger.LogInformation("Command '{CommandName}' handled successfully.", commandName);
+
+            return response;
         }
     }
 }
