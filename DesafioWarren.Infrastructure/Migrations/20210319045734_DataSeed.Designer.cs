7ddc8746b272daacab07dce90a7da383fcda910a
luiz motta <luizmotta01@hotmail.com>
2021-03-19 02:25:47 -0300

    feat: add data seed for test account and my account

--- DesafioWarren.Infrastructure/Migrations/20210319045734_DataSeed.Designer.cs
+++ DesafioWarren.Infrastructure/Migrations/20210319045734_DataSeed.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DesafioWarren.Infrastructure.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DesafioWarren.Infrastructure.Migrations
 {
     [DbContext(typeof(AccountsDbContext))]
-    partial class AccountsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210319045734_DataSeed")]
+    partial class DataSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
