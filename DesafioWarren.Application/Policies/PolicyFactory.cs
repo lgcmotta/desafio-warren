4eeebe5e988b16c786dce6b68398a117375f9691
luiz motta <luizmotta01@hotmail.com>
2021-03-19 13:15:23 -0300

    fix: change retry policy to forever

--- DesafioWarren.Application/Policies/PolicyFactory.cs
+++ DesafioWarren.Application/Policies/PolicyFactory.cs
@@ -8,13 +8,13 @@ namespace DesafioWarren.Application.Policies
     public class PolicyFactory
     {
         public static AsyncRetryPolicy CreateAsyncRetryPolicy(ILogger logger) => Policy.Handle<Exception>()
-            .WaitAndRetryAsync(10, sleepDurationProvider => TimeSpan.FromSeconds(5), (exception, timeSpan, attempt, context) =>
+            .WaitAndRetryForeverAsync(sleepDurationProvider => TimeSpan.FromSeconds(5), (exception, attempt, context) =>
             {
-                logger.Error(exception, "----------------- An exception of type {ExceptionType} with message {Message} has occurred. Retry attempt: {Attempt}, trying again in {Seconds} StackTrace: {StackTrace}"
+                logger.Error(exception, "----------------- An exception of type {ExceptionType} with message {Message} has occurred. Retry attempt: {Attempt}, trying again in {Seconds} seconds. StackTrace: {StackTrace}"
                     , exception.GetType().Name
                     , exception.Message
                     , attempt
-                    , timeSpan.Seconds
+                    , TimeSpan.FromSeconds(5)
                     , exception.StackTrace);
             });
     }
